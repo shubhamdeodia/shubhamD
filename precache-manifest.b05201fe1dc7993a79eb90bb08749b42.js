d091853cfb7c54aadea9654f55578bd772a31687
Shubham Deodia <deodia@ES2WXL-000284.wfs.global>
2020-04-08 13:03:56 +0200

    Updates

---
+++ precache-manifest.b05201fe1dc7993a79eb90bb08749b42.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3668404072fb16cc7d1b78d14a24d014",
+    "url": "/shubhamD/index.html"
+  },
+  {
+    "revision": "db1ff3998da3f2cc7188",
+    "url": "/shubhamD/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3e198831a8b96fc06a91",
+    "url": "/shubhamD/static/js/2.a84fcc4d.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/shubhamD/static/js/2.a84fcc4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db1ff3998da3f2cc7188",
+    "url": "/shubhamD/static/js/main.3c596e39.chunk.js"
+  },
+  {
+    "revision": "7fca1b0ba1176db9f8a7",
+    "url": "/shubhamD/static/js/runtime-main.df50f9f2.js"
+  },
+  {
+    "revision": "6afd24cf53cd4c892d5d435080feb5a0",
+    "url": "/shubhamD/static/media/Experience.6afd24cf.png"
+  },
+  {
+    "revision": "abe11cd6b7aa77944eb9234241a1a031",
+    "url": "/shubhamD/static/media/Skills.abe11cd6.png"
+  },
+  {
+    "revision": "cfa0a33bff0e8249ec3e2902a33ad8b7",
+    "url": "/shubhamD/static/media/avatar.cfa0a33b.jpg"
+  }
+]);
